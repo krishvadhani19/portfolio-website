ad6122f0ca94db7e687cf181988dbfe2fbfaafb4
krishvadhani19 <krish.vadhani@somaiya.edu>
2023-02-15 18:43:32 +0530

    Tech Stack Done

--- components/TechStack/TechStack.vue
+++ components/TechStack/TechStack.vue
@@ -1,14 +1,105 @@
 <template>
-  <div class="w-[70%] text-center my-32 mx-auto flex flex-col space-y-16">
-    <div class="text-6xl font-bold text-dark-700">Tech Stack</div>
+  <div class="w-[70%] my-32 mx-auto flex flex-col space-y-16">
+    <div class="text-6xl font-bold text-dark-700 text-center">Tech Stack</div>
 
-    <div class="flex lg:flex-row flex-col space-x-10"></div>
+    <div class="flex flex-col space-y-10 text-white">
+      <!-- 1. HTML -->
+      <div class="flex justify-between">
+        <div class="text-xl font-semibold w-[30%]">HTML</div>
+
+        <div class="w-[80%] h-3 bg-dark-400 rounded-lg my-auto">
+          <div
+            class="w-[100%] h-3 bg-gradient-to-r from-pink-500 via-cyan-500 to-purple-500 rounded-lg"
+          ></div>
+        </div>
+      </div>
+
+      <!-- 2. CSS, Tailwind CSS, Bootstrap -->
+      <div class="flex justify-between">
+        <div class="text-xl font-semibold w-[30%]">
+          CSS, Tailwind CSS, Bootstrap
+        </div>
+
+        <div class="w-[80%] h-3 bg-dark-400 rounded-lg my-auto">
+          <div
+            class="w-[100%] h-3 bg-gradient-to-r from-pink-500 via-cyan-500 to-purple-500 rounded-lg"
+          ></div>
+        </div>
+      </div>
+
+      <!-- 3. JavaScript -->
+      <div class="flex justify-between">
+        <div class="text-xl font-semibold w-[30%]">JavaScript</div>
+
+        <div class="w-[80%] h-3 bg-dark-400 rounded-lg my-auto">
+          <div
+            class="w-[100%] h-3 bg-gradient-to-r from-pink-500 via-cyan-500 to-purple-500 rounded-lg"
+          ></div>
+        </div>
+      </div>
+
+      <!-- 4. TypeScript -->
+      <div class="flex justify-between">
+        <div class="text-xl font-semibold w-[30%]">TypeScript</div>
+
+        <div class="w-[80%] h-3 bg-dark-400 rounded-lg my-auto">
+          <div
+            class="w-[65%] h-3 border border-pink-500 bg-gradient-to-r from-pink-500 via-cyan-500 to-purple-500 rounded-lg"
+          ></div>
+        </div>
+      </div>
+
+      <!-- 5. React -->
+      <div class="flex justify-between">
+        <div class="text-xl font-semibold w-[30%]">React Js</div>
+
+        <div class="w-[80%] h-3 bg-dark-400 rounded-lg my-auto">
+          <div
+            class="w-[80%] h-3 bg-gradient-to-r from-pink-500 via-cyan-500 to-purple-500 rounded-lg"
+          ></div>
+        </div>
+      </div>
+
+      <!-- 6. Nuxt -->
+      <div class="flex justify-between">
+        <div class="text-xl font-semibold w-[30%]">Nuxt Js</div>
+
+        <div class="w-[80%] h-3 bg-dark-400 rounded-lg my-auto">
+          <div
+            class="w-[85%] h-3 bg-gradient-to-r from-pink-500 via-cyan-500 to-purple-500 rounded-lg"
+          ></div>
+        </div>
+      </div>
+
+      <!-- 7. Node -->
+      <div class="flex justify-between">
+        <div class="text-xl font-semibold w-[30%]">Node Js</div>
+
+        <div class="w-[80%] h-3 bg-dark-400 rounded-lg my-auto">
+          <div
+            class="w-[75%] h-3 bg-gradient-to-r from-pink-500 via-cyan-500 to-purple-500 rounded-lg"
+          ></div>
+        </div>
+      </div>
+
+      <!-- 8. Express, Mongoose, MongoDB -->
+      <div class="flex justify-between">
+        <div class="text-xl font-semibold w-[30%]">
+          Express, Mongoose, MongoDB
+        </div>
+
+        <div class="w-[80%] h-3 bg-dark-400 rounded-lg my-auto">
+          <div
+            class="w-[85%] h-3 border border-dark-100 bg-gradient-to-r from-pink-500 via-cyan-500 to-purple-500 rounded-lg"
+          ></div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import GithubIcon from "~/components/icons/github.vue";
 export default {
-  components: { GithubIcon },
+  components: {},
 };
 </script>
