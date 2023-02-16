f04d36107ab0cfc2ab425836fd51d056a7bee514
krishvadhani19 <krish.vadhani@somaiya.edu>
2023-02-16 19:21:01 +0530

    some styling changes

--- components/Projects.vue
+++ components/Projects.vue
@@ -26,38 +26,11 @@
           ipsam?
         </div>
 
+       
         <a
-          class="text-xl bg-pink-500 w-[40%] mx-auto rounded-xl py-2 font-semibold"
+          class="w-[30%] mx-auto text-xl text-dark-700 font-bold text-center border border-primary-pink rounded-xl p-3 hover:bg-primary-pink ease-in-out duration-300 cursor-pointer"
           href="https://github.com/krishvadhani19/Social-Media-App"
-          >View Code</a
-        >
-      </div>
-      
-      <!-- Project1 -->
-      <div
-        class="flex flex-col border-2 border-pink-500 px-5 py-10 rounded-lg bg-gradient-to-r from-dark-100 via-dark-200 to-dark-100"
-      >
-        <!-- Title -->
-        <div class="text-3xl font-bold text-white">
-          Social Media Application
-        </div>
-
-        <!-- Sub Title -->
-        <div class="text-pink-500 font-bold text-base mb-6">
-          NodeJs, Mongoose, MongoDB
-        </div>
-
-        <!-- Description -->
-        <div class="w-[85%] mx-auto text-xl mb-5">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, sint
-          temporibus eius dicta culpa unde reiciendis quis cum dolorum debitis.
-          Tenetur magnam perferendis consequuntur quia provident quod cumque, a
-          ipsam?
-        </div>
-
-        <a
-          class="text-xl bg-pink-500 w-[40%] mx-auto rounded-xl py-2 font-semibold"
-          href="https://github.com/krishvadhani19/Social-Media-App"
+          target="_blank"
           >View Code</a
         >
       </div>
@@ -84,9 +57,42 @@
           ipsam?
         </div>
 
+       
         <a
-          class="text-xl bg-pink-500 w-[40%] mx-auto rounded-xl py-2 font-semibold"
+          class="w-[30%] mx-auto text-xl text-dark-700 font-bold text-center border border-primary-pink rounded-xl p-3 hover:bg-primary-pink ease-in-out duration-300 cursor-pointer"
           href="https://github.com/krishvadhani19/Social-Media-App"
+          target="_blank"
+          >View Code</a
+        >
+      </div>
+
+      <!-- Project1 -->
+      <div
+        class="flex flex-col border-2 border-pink-500 px-5 py-10 rounded-lg bg-gradient-to-r from-dark-100 via-dark-200 to-dark-100"
+      >
+        <!-- Title -->
+        <div class="text-3xl font-bold text-white">
+          Social Media Application
+        </div>
+
+        <!-- Sub Title -->
+        <div class="text-pink-500 font-bold text-base mb-6">
+          NodeJs, Mongoose, MongoDB
+        </div>
+
+        <!-- Description -->
+        <div class="w-[85%] mx-auto text-xl mb-5">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, sint
+          temporibus eius dicta culpa unde reiciendis quis cum dolorum debitis.
+          Tenetur magnam perferendis consequuntur quia provident quod cumque, a
+          ipsam?
+        </div>
+
+       
+        <a
+          class="w-[30%] mx-auto text-xl text-dark-700 font-bold text-center border border-primary-pink rounded-xl p-3 hover:bg-primary-pink ease-in-out duration-300 cursor-pointer"
+          href="https://github.com/krishvadhani19/Social-Media-App"
+          target="_blank"
           >View Code</a
         >
       </div>
@@ -113,8 +119,9 @@
         </div>
 
         <a
-          class="text-xl bg-pink-500 w-[40%] mx-auto rounded-xl py-2 font-semibold"
+          class="w-[30%] mx-auto text-xl text-dark-700 font-bold text-center border border-primary-pink rounded-xl p-3 hover:bg-primary-pink ease-in-out duration-300 cursor-pointer"
           href="https://github.com/krishvadhani19/Social-Media-App"
+          target="_blank"
           >View Code</a
         >
       </div>
